e85f8352c3d893567ca70a463f378dabbe209192
NicoAquino67 <nicoanriquez23@gmail.com>
2025-01-25 08:47:40 +0100

    Starting 25/1

--- Game1.cs
+++ Game1.cs
@@ -12,6 +12,8 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
     private Model _model;
+    private Model _skyboxModel;
+    private Texture2D _skyboxTexture;
     private Matrix _world = Matrix.CreateTranslation(0,0,0);
     private Matrix _view = Matrix.CreateLookAt(new Vector3(0,10,10),Vector3.Zero, Vector3.Up);
     private Matrix _projection;
@@ -35,13 +37,14 @@ public class Game1 : Game
         base.Initialize();
     }
 
-    private Texture2D _backgroundTexture;
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
         _model = Content.Load<Model>("Cube");
         // TODO: use this.Content to load your game content here
-        _backgroundTexture = Content.Load<Texture2D>("Skybox");
+        _skyboxModel = Content.Load<Model>("Cube");
+        _skyboxTexture = Content.Load<Texture2D>("skybox");
+
         for (int x = -5; x < 5; x++){
             for (int y = 0;y < 2; y++){
                 for (int z = -5; z < 5; z++){
@@ -50,6 +53,20 @@ public class Game1 : Game
                 }
             }
         }
+    }
+    private void DrawSkybox(){
+        GraphicsDevice.RasterizerState = new RasterizerState{CullMode = CullMode.CullCounterClockwiseFace};
+        foreach (var mesh in _skyboxModel.Meshes){
+            foreach (BasicEffect effect in mesh.Effects){
+                effect.World = Matrix.CreateTranslation(Vector3.Zero);
+                effect.View = _player.GetViewMatrix();
+                effect.Projection = _projection;
+                effect.TextureEnabled = true;
+                effect.Texture = _skyboxTexture;
+            }
+            mesh.Draw();
+        }
+        GraphicsDevice.RasterizerState = RasterizerState.CullCounterClockwise;
     }
 
     protected override void Update(GameTime gameTime)
@@ -67,6 +84,7 @@ public class Game1 : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
         _view = _player.GetViewMatrix();
+        DrawSkybox();
         foreach(var block in _blocks){
             foreach (var mesh in block.BlockModel.Meshes){
                 foreach (BasicEffect effect in mesh.Effects){
